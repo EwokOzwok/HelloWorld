9a559743c9c1bf44952f2caa8b6eda2b9d6ab0f3
EwokOzwok <eozmat@albany.edu>
2024-07-08 14:13:25 -0400

    demo app

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,33 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+
+
+  observeEvent(input$mybutton,{
+    text = "This text will be displayed"
+
+    output$text<-renderUI({
+      tagList(
+        renderText(text)
+      )
+    })
+
+    output$clearbutton<-renderUI({
+      tagList(
+        f7Button("clear", "Delete Text")
+      )
+    })
+
+
+  })
+
+
+
+  observeEvent(input$clear,{
+    output$clearbutton<-renderUI({})
+    output$text<-renderUI({})
+
+  })
+
+
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -3,14 +3,71 @@
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import shinyMobile
 #' @noRd
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      h1("HelloWorld")
+    shinyMobile::f7Page(
+      title = "Hello World",
+      options = list(theme=c("auto"), dark=TRUE, preloader = F,  pullToRefresh=F),
+      allowPWA=TRUE,
+
+
+      f7TabLayout(
+        # panels are not mandatory. These are similar to sidebars
+        navbar = f7Navbar(
+          title= "Hello World App"),
+        # f7Tabs is a special toolbar with included navigation
+        f7Tabs(
+          animated = TRUE,
+          id = "tabs",
+          f7Tab(
+            tabName = "Tab1",
+            icon = f7Icon("house_fill"),
+            active = TRUE,
+            hidden= F,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("Welcome to the Hello World App"),side=c("center")),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+          ),
+
+
+          f7Tab(
+            tabName = "tab2",
+            icon = f7Icon("airplane"),
+            active = F,
+            hidden=F,
+            f7Block(
+              f7Shadow(
+                intensity = 5,
+                hover = TRUE,
+                f7Card(
+                  f7Align(h2("This is my second tab"),side=c("center")),
+                  br(),
+                  hr(),
+                  br(),
+                  f7Button("mybutton", "Click me", shadow = T, rounded = T),
+                  br(),
+                  uiOutput("text"),
+                  br(),
+                  uiOutput("clearbutton"),
+                  footer = NULL,
+                  hairlines = F, strong = T, inset = F, tablet = FALSE)
+              )
+            )
+          )
+        )
+      )
     )
   )
 }
